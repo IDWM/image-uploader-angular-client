db64f754b87cc06e944e4d6984b5613258d3aeb2
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-03-16 00:06:10 -0300

    feat: create form for uploading posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TextInputComponent } from './_components/text-input/text-input.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { DropzoneInputComponent } from './_components/dropzone-input/dropzone-input.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    ReactiveFormsModule,
+    TextInputComponent,
+    DropzoneInputComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'Image Uploader';
+export class AppComponent implements OnInit {
+  formGroup: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.formGroup = this.formBuilder.nonNullable.group({
+      title: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+        ],
+      ],
+      description: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(100),
+        ],
+      ],
+      image: [null, Validators.required],
+    });
+  }
+
+  uploadPost(): void {}
+
+  cancelUpload(): void {
+    this.formGroup.reset();
+  }
 }
